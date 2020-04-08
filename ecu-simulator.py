62cdda3784fcb735f716d25403212efa99883f18
Sergey Shcherbakov <shchers@gmail.com>
2020-04-08 17:33:52 +0300

    Added advanced logging support
    Signed-off-by: Sergey Shcherbakov <shchers@gmail.com>

--- ecu-simulator.py
+++ ecu-simulator.py
@@ -4,72 +4,75 @@ from __future__ import print_function
 
 from random import randint
 
+import logging as log
+import getopt, sys
+
 import can
 from can.bus import BusState
 
 def service1(bus, msg):
     if msg.data[2] == 0x00:
-        print(">> Caps")
+        log.debug(">> Caps")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x06, 0x41, 0x00, 0xBF, 0xDF, 0xB9, 0x91],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x04:
-        print(">> Calculated engine load")
+        log.debug(">> Calculated engine load")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x04, 0x20],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x05:
-        print(">> Engine coolant temperature")
+        log.debug(">> Engine coolant temperature")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x05, randint(88 + 40, 95 + 40)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x0B:
-        print(">> Intake manifold absolute pressure")
+        log.debug(">> Intake manifold absolute pressure")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x04, 0x41, 0x0B, randint(10, 40)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x0C:
-        print(">> RPM")
+        log.debug(">> RPM")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x04, 0x41, 0x0C, randint(18, 70), randint(0, 255)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x0D:
-        print(">> Speed")
+        log.debug(">> Speed")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x0D, randint(40, 60)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x0F:
-        print(">> Intake air temperature")
+        log.debug(">> Intake air temperature")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x0F, randint(60, 64)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x10:
-        print(">> MAF air flow rate")
+        log.debug(">> MAF air flow rate")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x04, 0x41, 0x10, 0x00, 0xFA],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x11:
-        print(">> Throttle position")
+        log.debug(">> Throttle position")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x11, randint(20, 60)],
           is_extended_id=False)
         bus.send(msg)
     elif msg.data[2] == 0x33:
-        print(">> Absolute Barometric Pressure")
+        log.debug(">> Absolute Barometric Pressure")
         msg = can.Message(arbitration_id=0x7e8,
           data=[0x03, 0x41, 0x33, randint(20, 60)],
           is_extended_id=False)
         bus.send(msg)
     else:
-        print("!!! Service 1, unknown code", msg.data[2])
+        log.warning("!!! Service 1, unknown code 0x%02x", msg.data[2])
 
 
 def receive_all():
@@ -89,11 +92,42 @@ def receive_all():
                 if msg.arbitration_id == 0x7df and msg.data[1] == 0x01:
                     service1(bus, msg)
                 else:
-                    print("Unknown ID", msg.arbitration_id, "or service code", msg.data[1])
+                    log.warning("Unknown ID %d or service code 0x%02x", msg.arbitration_id, msg.data[1])
 
     except KeyboardInterrupt:
         pass
 
+def usage():
+    # DOTO: implement
+    pass
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "l:v", ["loglevel="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    loglevel = "INFO"
+
+    for o, a in opts:
+        if o == "-v":
+            loglevel = "DEBUG"
+        elif o in ("-l", "--loglevel"):
+            loglevel = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        else:
+            assert False, "unhandled option"
+
+    numeric_level = getattr(log, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    log.basicConfig(level=numeric_level)
+    receive_all()
 
 if __name__ == "__main__":
-    receive_all()
+    main();
